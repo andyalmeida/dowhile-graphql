fd4db96cb3095ba09fa8d1d8ef98fcfe931cfa9e
André Almeida <andalvalmeida@gmail.com>
2020-12-14 21:59:46 -0300

    Init repository

---
+++ src/graphql/category/CategoryResolver.ts
@@ -0,0 +1,30 @@
+import { Arg, Field, InputType, Mutation, Query, Resolver } from 'type-graphql';
+import CategorySchema from '../../model/CategorySchema';
+import Category from './Category';
+
+@InputType()
+class CategoryInput {
+  @Field()
+  description: string;
+  @Field()
+  name: string;
+}
+
+@Resolver(Category)
+class CategoryResolver {
+
+  @Query(() => [Category])
+  async categories() {
+    const categories = await CategorySchema.find();
+    return categories;
+  }
+
+  @Mutation(() => Category)
+  async createCategory(@Arg('categoryInput') categoryInput: CategoryInput) {
+    const category = await CategorySchema.create(categoryInput);
+    return category;
+  }
+
+}
+
+export default CategoryResolver;
---
+++ src/utils/connection.ts
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb+srv://andre:8lQ9dXbdhhonazIs@cluster0.fuqbj.mongodb.net/rocket?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
